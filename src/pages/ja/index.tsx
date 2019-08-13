60cec2863b3a684bfcf6f650f44562b5e6e8a5fa
tuzimoe <a@tuzi.moe>
2019-08-14 10:26:01 +1200

    en and ja and team

---
+++ src/pages/ja/index.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import Helmet from 'react-helmet';
+
+import { Intro } from 'components/enintro/Intro';
+import { Highlight } from 'components/enintro/Highlight';
+import { BlockText } from 'components/block-text/BlockText';
+
+export default () => (
+  <>
+    <Helmet title="Hello" />
+
+    <Intro>
+      Hi! This is <Highlight>Group Of Otaku Shenzhen Network Technology Co., Ltd. 🤩</Highlight><br/>We're from <Highlight>Guangdong Shenzhen 🇨🇳</Highlight>.
+      We connect Developers,commercial organizations,service providers. Explore personalized industry solutions. Through geographic empowerment, we work with users to explore a bigger world and possible🌍.<br/>
+    </Intro>
+
+    <BlockText
+      heading="Company's Product"
+      description={(
+        <>
+          Dooonut is an LBS social app launched by us.BFF Only👭Miss you buddy ⚽️, girlfriends 💅? Miss you buddy ⚽️, girlfriends 💅? Find them in the donuts. We provide unnecessarily flaws in freezing 😵 and blur positions. It also ensures that your location is safe and will not be obtained by strangers. New friends? Shake and add friends! 🙋<a href="https://www.dooonut.com" target="_blank" rel="noopener noreferrer">www.dooonut.com</a>
+        </>
+      )}
+    />
+  </>
+);
