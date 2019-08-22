283cfc2604f25f1ba8483747d11ee7529b3f6688
TuziMoe <a@tuzi.moe>
2019-08-22 12:43:50 +1200

    Update team.tsx

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -12,11 +12,37 @@ export default () => (
       关于团队 
     </Intro>
     <BlockText
-      heading="公司产品"
+      heading="刘佳其"
       description={(
         <>
-          <li>Dooonut 甜甜圈 是一款由我们推出的 LBS 社交 app。好友才能用👭想念你的哥们⚽️、闺蜜💅？在甜甜圈里找到他们。我们提供冻结❄️与模糊😵位置减少不必要的尴尬。也保证了你的位置安全，不会被陌生人得到。新交了朋友？摇一摇加好友吧！🙋<a href="https://www.dooonut.com" target="_blank" rel="noopener noreferrer">dooonut.com</a></li><br />
-          <li>Auriga 阿瑞嘉 是我们公司制作的动漫真人偶像团体组合，是基于新生代互联网思维和用户参与感精神、从线上到线下与可面对面偶像的新型公司特有运营理念的近距离养成式动漫真人偶像团体，结合动漫与真人配音偶像，充分运用互联网和社交网络时代的丰富无限想象力，绝佳的创新型和大胆的前瞻性理念，相碰撞结合制造出一个动漫产业与真人偶像产业的全方面企划。<a href="https://www.aruiga.live" target="_blank" rel="noopener noreferrer">auriga.live</a></li>
+          <li>其实我是传说中的全干攻城狮，会干的我全干，不会干的我也干了</li>
+          </>
+        
+      )}
+    />
+    <BlockText
+      heading="吴凡飞"
+      description={(
+        <>
+          <li>追梦的理想主义者。大学生，永远喜欢纸片人.jpg</li>
+          </>
+        
+      )}
+    />
+    <BlockText
+      heading="宋树斌"
+      description={(
+        <>
+          <li>一般通过普通学生，境界线就在不远处的中二诊所角落</li>
+          </>
+        
+      )}
+    />
+    <BlockText
+      heading="谢梦达"
+      description={(
+        <>
+          <li>告诉你们一个秘密，其实我是全干攻城狮他爹</li>
           </>
         
       )}
