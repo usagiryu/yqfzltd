d87307a956f8790e130f7b341b77d9e9383e6598
TuziMoe <a@tuzi.moe>
2019-08-22 12:57:10 +1200

    Update team.tsx

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -12,37 +12,13 @@ export default () => (
       关于团队 
     </Intro>
     <BlockText
-      heading="刘佳其"
+      heading="团队成员"
       description={(
         <>
-          <li>其实我是传说中的全干攻城狮，会干的我全干，不会干的我也干了</li>
-          </>
-        
-      )}
-    />
-    <BlockText
-      heading="吴凡飞"
-      description={(
-        <>
-          <li>追梦的理想主义者。大学生，永远喜欢纸片人.jpg</li>
-          </>
-        
-      )}
-    />
-    <BlockText
-      heading="宋树斌"
-      description={(
-        <>
-          <li>一般通过普通学生，境界线就在不远处的中二诊所角落</li>
-          </>
-        
-      )}
-    />
-    <BlockText
-      heading="谢梦达"
-      description={(
-        <>
-          <li>告诉你们一个秘密，其实我是全干攻城狮他爹</li>
+          <li>刘佳其 · 其实我是传说中的全干攻城狮，会干的我全干，不会干的我也干了</li>
+          <li>吴凡飞 · 追梦的理想主义者。大学生，永远喜欢纸片人.jpg</li>
+          <li>宋树斌 · 一般通过普通学生，境界线就在不远处的中二诊所角落</li>
+          <li>谢梦达 · 告诉你们一个秘密，其实我是全干攻城狮他爹</li>
           </>
         
       )}
